24ba557660d323ab4e54c2a47a7f1fbcd9dc45f0
Yannick Debrée <contact@yannickdebree.com>
2021-11-26 23:30:33 +0100

    build: lint added

--- src/app/case-color/case-color.component.ts
+++ src/app/case-color/case-color.component.ts
@@ -1,34 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Board, Case } from '../domain';
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-case-color',
   templateUrl: './case-color.component.html',
-  styleUrls: ['./case-color.component.scss']
+  styleUrls: ['./case-color.component.scss'], changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CaseColorComponent {
-  private index: number | undefined;
   private board = new Board();
   private cases = this.board.getCases();
 
   currentCase: Case | undefined;
-  error: string | undefined;
 
-  constructor() {
+  constructor(private router: Router, private matSnackBar: MatSnackBar) {
     this.selectRandomCase();
   }
 
   selectRandomCase() {
-    this.index = Math.floor(Math.random() * this.cases.length);
-    this.currentCase = this.cases[this.index];
+    const randomIndex = Math.floor(Math.random() * this.cases.length);
+    this.currentCase = this.cases[randomIndex];
   }
 
   onSubmit(answer: boolean) {
-    this.error = '';
+    this.matSnackBar.dismiss();
+
     if (this.currentCase?.getColor() !== answer) {
-      this.error = `${this.currentCase?.toString()} est de couleur ${answer ? 'noire' : 'blanche'}.`;
+      this.matSnackBar.open(
+        `${this.currentCase?.toString()} est de couleur ${answer ? 'blanche' : 'noire'}.`, 'OK', { duration: 2000 }
+      );
     }
     this.selectRandomCase();
   }
 
+  onNavigationBefore() {
+    this.router.navigate(['..']);
+  }
 }
--- src/app/case-color/case-color.module.ts
+++ src/app/case-color/case-color.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatIconModule } from '@angular/material/icon'
 import { CaseColorRoutingModule } from './case-color-routing.module';
 import { CaseColorComponent } from './case-color.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { CaseColorComponent } from './case-color.component';
   ],
   imports: [
     CommonModule,
-    CaseColorRoutingModule
+    CaseColorRoutingModule, MatIconModule, FlexLayoutModule, MatButtonModule, MatSnackBarModule
   ]
 })
 export class CaseColorModule { }
