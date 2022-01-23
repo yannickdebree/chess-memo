2094797a6d1e2d051c22db7a7c6c8967f70691ac
Yannick Debrée <contact@yannickdebree.com>
2022-01-23 18:42:37 +0100

    feat: language change added

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+enum Language {
+  EN = 'EN',
+  FR = 'FR'
+}
+
 @Component({
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomepageComponent {
+  languages = [Language.EN, Language.FR].map(l => ({ letter: l, active: l === Language.FR }))
+
+  changeLanguage(lang: Language) {
+    this.languages.forEach(language => {
+      language.active = language.letter === lang;
+    })
+  }
 }
